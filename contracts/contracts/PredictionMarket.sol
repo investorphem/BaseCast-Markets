117762314de678ac91cdc0c78eff0793442cd9fc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-05 09:10:19 +0100

    Update PredictionMarket.sol

--- contracts/contracts/PredictionMarket.sol
+++ contracts/contracts/PredictionMarket.sol
@@ -10,7 +10,7 @@ contract PredictionMarket {
 
     string public question;
     uint256 public endTime;
-    address ulc creator;
+    addres ulc creator;
     Outcompblic outcome;
     bool ucelved;
     mappingadess =>uint256) public yesShares;
