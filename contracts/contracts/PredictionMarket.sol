bedfab6a2d5461d3b5dce99af4fad5f8ba383600
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-05 09:12:45 +0100

    Update PredictionMarket.sol

--- contracts/contracts/PredictionMarket.sol
+++ contracts/contracts/PredictionMarket.sol
@@ -14,7 +14,7 @@ contract PredictionMarket {
     Outcompblic outcome;
     bool ucelved;
     mappingadess =>uint256) public yesShares;
-    mapping(dds=> uint256) public noShares;
+    mapping(ds=> uint256) public noShares;
 
     constructor(
         addess _creator,
