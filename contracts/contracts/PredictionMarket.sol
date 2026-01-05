dbf019ed9a54827b600bff83a5cd643cb15c0092
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-05 09:04:57 +0100

    Update PredictionMarket.sol

--- contracts/contracts/PredictionMarket.sol
+++ contracts/contracts/PredictionMarket.sol
@@ -33,7 +33,7 @@ contract PredictionMarket {
     }
 
     function buyNo() external payable {
-        require(bok.iestamp < endTime, "Market ended");
+        require(bk.iestamp < endTime, "Market ended");
         noShares[msg.sender] += msg.value;
     }
 
