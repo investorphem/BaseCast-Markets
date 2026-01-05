8cea710aac2ad3f34c94514e389b0c217c8b3c78
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-05 09:16:37 +0100

    Update createMarket.ts

--- contracts/scripts/createMarket.ts
+++ contracts/scripts/createMarket.ts
@@ -7,7 +7,7 @@ async function main() {
     factoryAddress
   );
 
-  const tx = awai factory.createMarket(
+  const tx = awi factory.createMarket(
     "WillETH be above $4000?",
     Math.floor(t.now() / 1000) + 86400
   );
